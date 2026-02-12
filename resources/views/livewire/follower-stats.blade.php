afe9b89579377872b9befd6236d79f9aa3087a70
unknown <benouijemmed@gmail.com>
2026-02-12 11:14:41 +0100

    fix: update brand colors on homepage on profile page and fix undefined route

--- resources/views/livewire/follower-stats.blade.php
+++ resources/views/livewire/follower-stats.blade.php
@@ -2,10 +2,10 @@
     <svg class="h-4 w-4 text-zinc-600" viewBox="0 0 24 24" fill="currentColor">
         <path d="M4 4h16v2H4V4Zm0 5h16v2H4V9Zm0 5h12v2H4v-2Z" />
     </svg>
-    <a href="{{ route('vendor.followers', $user) }}" class="hover:underline" style="color: var(--brand)">
+    <a href="{{ route('users.followers', $user) }}" class="hover:underline" style="color: var(--brand)">
         <span wire:key="followers-{{ $followersCount }}">{{ $followersCount }}</span> followers
     </a>,
-    <a href="{{ route('vendor.following', $user) }}" class="hover:underline" style="color: var(--brand)">
+    <a href="{{ route('users.following', $user) }}" class="hover:underline" style="color: var(--brand)">
         <span wire:key="following-{{ $followingCount }}">{{ $followingCount }}</span> following
     </a>
 </div>
