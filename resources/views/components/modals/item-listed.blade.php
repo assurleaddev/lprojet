4efcb54c3969bf36770e5eb8af7166a3e67db6cf
mohammed benouijem <benouijemmed@gmail.com>
2026-04-21 18:00:48 +0200

    fix: modal white page on outside click - use backdrop click handler instead of click.away

--- resources/views/components/modals/item-listed.blade.php
+++ resources/views/components/modals/item-listed.blade.php
@@ -14,15 +14,19 @@
 @endphp
 
 <div x-data="itemListedModal(@js($initialProduct))"
-    @item-listed.window="show = true; product = $event.detail.product" x-show="show" class="relative z-[100]"
-    aria-labelledby="modal-title" role="dialog" aria-modal="true" x-cloak>
+    @item-listed.window="show = true; product = $event.detail.product"
+    aria-labelledby="modal-title" role="dialog" aria-modal="true">
 
-    <div x-show="show" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0"
+    {{-- Backdrop: click closes the modal --}}
+    <div x-show="show"
+        x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0"
         x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200"
         x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
-        class="fixed inset-0 z-[100] bg-gray-500 opacity-75 transition-opacity backdrop-blur-sm"></div>
+        @click="show = false"
+        class="fixed inset-0 z-[100] bg-gray-500/75 backdrop-blur-sm"
+        x-cloak></div>
 
-    <div class="fixed inset-0 z-[101] w-screen overflow-y-auto">
+    <div x-show="show" class="fixed inset-0 z-[101] w-screen overflow-y-auto" x-cloak>
         <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <div x-show="show" x-transition:enter="ease-out duration-300 transform"
                 x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
@@ -30,7 +34,7 @@
                 x-transition:leave="ease-in duration-200 transform"
                 x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
                 x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                @click.away="show = false"
+                @click.stop
                 class="relative transform overflow-hidden rounded-xl bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-md">
 
                 <!-- Decorative Elements -->
