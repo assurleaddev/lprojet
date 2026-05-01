55b94d03cc2c0eb3b1ae18f826d5927c4d6b2dcf
mohammed benouijem <benouijemmed@gmail.com>
2026-05-01 09:56:11 +0200

    fix: allow composer superuser and remove optimize-autoloader in deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo "==> Pulling latest code..."
 git pull origin main
 
 echo "==> Installing dependencies..."
-composer install --no-dev --optimize-autoloader --no-interaction
+COMPOSER_ALLOW_SUPERUSER=1 composer install --no-dev --no-interaction
 
 echo "==> Running migrations..."
 php artisan migrate --force --no-interaction
