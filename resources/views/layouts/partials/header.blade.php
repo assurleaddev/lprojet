154ede36b59100a4ef282ecf76f4e633eaa0c753
mohammed benouijem <benouijemmed@gmail.com>
2026-05-03 18:09:17 +0200

    fix: remove help center icon from header

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -223,14 +223,6 @@
                     </button>
                 @endif
                 <a href="{{ route('items.create') }}" class="px-4 py-2 rounded text-white font-bold text-sm whitespace-nowrap shrink-0 text-center bg-gray-900 hover:bg-gray-800">{{ __('Sell') }}</a>
-                <a href="#" class="text-gray-600 hover:text-black shrink-0 flex items-center md:block" aria-label="Help">
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
-                            d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z">
-                        </path>
-                    </svg>
-                    <span class="ml-4 font-medium md:hidden">{{ __('Help Center') }}</span>
-                </a>
 
                 <!-- Language Dropdown -->
                 <div class="relative shrink-0 flex flex-col md:block" x-data="{ open: false }">
